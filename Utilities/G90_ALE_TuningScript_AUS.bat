6ceeb05d9e92c23def8a304464afba6b0c1466cb
Rob <roblyonz@gmail.com>
2022-08-04 21:50:37 +1000

    G90 tuning offset lowered
    The G90 seems to tune best when activated slightly below the desired frequency. This also has the advantage of avoiding potential QRM.

--- Utilities/G90_ALE_TuningScript_AUS.bat
+++ Utilities/G90_ALE_TuningScript_AUS.bat
@@ -5,103 +5,103 @@ ECHO off
 
 set COM=COM11
 
-set /a freq=1843000-3000
+set /a freq=1843000-1000
 rigctl -m 370 -r %COM% F %freq%
 rem ATU TUNE command
 rigctl -m 370 -r %COM% w \0xFE\0xFE\0x88\0xE0\0x1C\0x01\0x02\0xFD
 timeout /t 15
 
-set /a freq=3596000-3000
+set /a freq=3596000-1000
 rigctl -m 370 -r %COM% F %freq%
 rem ATU TUNE command
 rigctl -m 370 -r %COM% w \0xFE\0xFE\0x88\0xE0\0x1C\0x01\0x02\0xFD
 timeout /t 15
 
-set /a freq=3600500-3000
+set /a freq=3600500-1000
 rigctl -m 370 -r %COM% F %freq%
 rem ATU TUNE command
 rigctl -m 370 -r %COM% w \0xFE\0xFE\0x88\0xE0\0x1C\0x01\0x02\0xFD
 timeout /t 15
 
-set /a freq=7102000-3000
+set /a freq=7102000-1000
 rigctl -m 370 -r %COM% F %freq%
 rem ATU TUNE command
 rigctl -m 370 -r %COM% w \0xFE\0xFE\0x88\0xE0\0x1C\0x01\0x02\0xFD
 timeout /t 15
 
-set /a freq=7185000-3000
+set /a freq=7185000-1000
 rigctl -m 370 -r %COM% F %freq%
 rem ATU TUNE command
 rigctl -m 370 -r %COM% w \0xFE\0xFE\0x88\0xE0\0x1C\0x01\0x02\0xFD
 timeout /t 15
 
-set /a freq=10131000-3000
+set /a freq=10131000-1000
 rigctl -m 370 -r %COM% F %freq%
 rem ATU TUNE command
 rigctl -m 370 -r %COM% w \0xFE\0xFE\0x88\0xE0\0x1C\0x01\0x02\0xFD
 timeout /t 15
 
-set /a freq=10145500-3000
+set /a freq=10145500-1000
 rigctl -m 370 -r %COM% F %freq%
 rem ATU TUNE command
 rigctl -m 370 -r %COM% w \0xFE\0xFE\0x88\0xE0\0x1C\0x01\0x02\0xFD
 timeout /t 15
 
-set /a freq=14109000-3000
+set /a freq=14109000-1000
 rigctl -m 370 -r %COM% F %freq%
 rem ATU TUNE command
 rigctl -m 370 -r %COM% w \0xFE\0xFE\0x88\0xE0\0x1C\0x01\0x02\0xFD
 timeout /t 15
 
-set /a freq=14346000-3000
+set /a freq=14346000-1000
 rigctl -m 370 -r %COM% F %freq%
 rem ATU TUNE command
 rigctl -m 370 -r %COM% w \0xFE\0xFE\0x88\0xE0\0x1C\0x01\0x02\0xFD
 timeout /t 15
 
-set /a freq=18106000-3000
+set /a freq=18106000-1000
 rigctl -m 370 -r %COM% F %freq%
 rem ATU TUNE command
 rigctl -m 370 -r %COM% w \0xFE\0xFE\0x88\0xE0\0x1C\0x01\0x02\0xFD
 timeout /t 15
 
-set /a freq=18117500-3000
+set /a freq=18117500-1000
 rigctl -m 370 -r %COM% F %freq%
 rem ATU TUNE command
 rigctl -m 370 -r %COM% w \0xFE\0xFE\0x88\0xE0\0x1C\0x01\0x02\0xFD
 timeout /t 15
 
-set /a freq=21096000-3000
+set /a freq=21096000-1000
 rigctl -m 370 -r %COM% F %freq%
 rem ATU TUNE command
 rigctl -m 370 -r %COM% w \0xFE\0xFE\0x88\0xE0\0x1C\0x01\0x02\0xFD
 timeout /t 15
 
-set /a freq=21432500-3000
+set /a freq=21432500-1000
 rigctl -m 370 -r %COM% F %freq%
 rem ATU TUNE command
 rigctl -m 370 -r %COM% w \0xFE\0xFE\0x88\0xE0\0x1C\0x01\0x02\0xFD
 timeout /t 15
 
-set /a freq=24926000-3000
+set /a freq=24926000-1000
 rigctl -m 370 -r %COM% F %freq%
 rem ATU TUNE command
 rigctl -m 370 -r %COM% w \0xFE\0xFE\0x88\0xE0\0x1C\0x01\0x02\0xFD
 timeout /t 15
 
-set /a freq=24932000-3000
+set /a freq=24932000-1000
 rigctl -m 370 -r %COM% F %freq%
 rem ATU TUNE command
 rigctl -m 370 -r %COM% w \0xFE\0xFE\0x88\0xE0\0x1C\0x01\0x02\0xFD
 timeout /t 15
 
-set /a freq=28146000-3000
+set /a freq=28146000-1000
 rigctl -m 370 -r %COM% F %freq%
 rem ATU TUNE command
 rigctl -m 370 -r %COM% w \0xFE\0xFE\0x88\0xE0\0x1C\0x01\0x02\0xFD
 timeout /t 15
 
-set /a freq=28312500-3000
+set /a freq=28312500-1000
 rigctl -m 370 -r %COM% F %freq%
 rem ATU TUNE command
 rigctl -m 370 -r %COM% w \0xFE\0xFE\0x88\0xE0\0x1C\0x01\0x02\0xFD
